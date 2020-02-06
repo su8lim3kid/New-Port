d28fcf9bded67b2d67f843643efa0533ef16d75b
su8lim3kid <aldereteangel21@gmail.com>
2020-02-06 10:04:26 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+document.querySelector('.hamburger-menu').addEventListener('click', () => {
+  document.querySelector('.nav-wrapper').classList.toggle('change');
+});
+
+document.querySelector('.banner-btn').addEventListener('click', () => {
+  document.querySelector('.nav-wrapper').classList.toggle('change');
+});
